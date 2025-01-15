692121767fa351d02c0bc4bcb048f62d4c6fd3cb
DipanNama <dipannama91@gmail.com>
2025-01-16 01:16:11 +0530

    Fixed some minor bugs

--- js/script.js
+++ js/script.js
@@ -2,17 +2,27 @@ const toggleSidebarOpen = document.querySelector('#toggle-sidebar-open');
 const toggleSidebarClose = document.querySelector('#toggle-sidebar-close');
 const nav = document.querySelector('#nav');
 
-toggleSidebarOpen.addEventListener('click',() => {
-    document.querySelector('#sidebar').classList.remove('hidden');
-    nav.classList.add('hidden');
+toggleSidebarOpen.addEventListener('click', () => {
+  document.querySelector('#sidebar').classList.remove('hidden');
+  nav.classList.add('hidden');
 });
 
-toggleSidebarClose.addEventListener('click',() => {
-    document.querySelector('#sidebar').classList.add('hidden');
-    nav.classList.remove('hidden');
+toggleSidebarClose.addEventListener('click', () => {
+  document.querySelector('#sidebar').classList.add('hidden');
+  nav.classList.remove('hidden');
 });
 
+const msgBlocks = document.querySelectorAll('.msg-block');
 
+msgBlocks.forEach(msgBlock => {
+  msgBlock.addEventListener("mouseover", () => {
+    msgBlock.style.animationPlayState = "paused";
+  });
+
+  msgBlock.addEventListener("mouseout", () => {
+    msgBlock.style.animationPlayState = "running";
+  });
+});
 
 tailwind.config = {
   theme: {
