1d835428dab8126953c5552a84717b535d5e7cc6
DipanNama <dipannama91@gmail.com>
2025-01-15 23:56:37 +0530

    Fixed home page bugs and made it responsive

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 const toggleSidebarOpen = document.querySelector('#toggle-sidebar-open');
 const toggleSidebarClose = document.querySelector('#toggle-sidebar-close');
+const nav = document.querySelector('#nav');
 
 toggleSidebarOpen.addEventListener('click',() => {
     document.querySelector('#sidebar').classList.remove('hidden');
+    nav.classList.add('hidden');
 });
 
 toggleSidebarClose.addEventListener('click',() => {
     document.querySelector('#sidebar').classList.add('hidden');
+    nav.classList.remove('hidden');
 });
 
 
